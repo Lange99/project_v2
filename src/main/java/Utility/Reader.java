24779ca5ae990a670ee36c84c56d1997a98c455d
ilNick99 <n.giacomini001@studenti.unibs.it>
2021-05-19 11:39:25 +0200

    traduzione fatta

--- src/main/java/Utility/Reader.java
+++ src/main/java/Utility/Reader.java
@@ -1,175 +1,138 @@
-//package Utility;
-//
-//
-//import java.util.*;
-//
-//public class Reader {
-//
-//    private static Scanner lettore = creaScanner();
-//
-//    private final static String ERRORE_FORMATO = "Attenzione: il dato inserito non e' nel formato corretto";
-//    private final static String ERRORE_MINIMO = "Attenzione: e' richiesto un valore maggiore o uguale a ";
-//    private final static String ERRORE_STRINGA_VUOTA = "Attenzione: non hai inserito alcun carattere";
-//    private final static String ERRORE_MASSIMO = "Attenzione: e' richiesto un valore minore o uguale a ";
-//    private final static String MESSAGGIO_AMMISSIBILI = "Attenzione: i caratteri ammissibili sono: ";
-//
-//    private final static char RISPOSTA_SI = 'S';
-//    private final static char RISPOSTA_NO = 'N';
-//
-//
-//    private static Scanner creaScanner() {
-//        Scanner creato = new Scanner(System.in);
-//        //creato.useDelimiter(System.getProperty("line.separator"));
-//        creato.useDelimiter(System.lineSeparator() + "|\n");
-//        return creato;
-//    }
-//
-//    public static String ReadString(String messaggio) {
-//        System.out.print(messaggio);
-//        return lettore.next();
-//    }
-//
-//    public static String readNotEmpityString(String messaggio) {
-//        boolean finito = false;
-//        String lettura = null;
-//        do {
-//            lettura = ReadString(messaggio);
-//            lettura = lettura.trim();
-//            if (lettura.length() > 0)
-//                finito = true;
-//            else
-//                System.out.println(ERRORE_STRINGA_VUOTA);
-//        } while (!finito);
-//
-//        return lettura;
-//    }
-//
-//    public static char leggiChar(String messaggio) {
-//        boolean finito = false;
-//        char valoreLetto = '\0';
-//        do {
-//            System.out.print(messaggio);
-//            String lettura = lettore.next();
-//            if (lettura.length() > 0) {
-//                valoreLetto = lettura.charAt(0);
-//                finito = true;
-//            } else {
-//                System.out.println(ERRORE_STRINGA_VUOTA);
-//            }
-//        } while (!finito);
-//        return valoreLetto;
-//    }
-//
-//    public static char leggiUpperChar(String messaggio, String ammissibili) {
-//        boolean finito = false;
-//        char valoreLetto = '\0';
-//        do {
-//            valoreLetto = leggiChar(messaggio);
-//            valoreLetto = Character.toUpperCase(valoreLetto);
-//            if (ammissibili.indexOf(valoreLetto) != -1)
-//                finito = true;
-//            else
-//                System.out.println(MESSAGGIO_AMMISSIBILI + ammissibili);
-//        } while (!finito);
-//        return valoreLetto;
-//    }
-//
-//
-//    public static int readNumber(String messaggio) {
-//        boolean finito = false;
-//        int valoreLetto = 0;
-//        do {
-//            System.out.print(messaggio);
-//            try {
-//                valoreLetto = lettore.nextInt();
-//                finito = true;
-//            } catch (InputMismatchException e) {
-//                System.out.println(ERRORE_FORMATO);
-//                String daButtare = lettore.next();
-//            }
-//        } while (!finito);
-//        return valoreLetto;
-//    }
-//
-//    public static int leggiInteroPositivo(String messaggio) {
-//        return leggiInteroConMinimo(messaggio, 1);
-//    }
-//
-//    public static int leggiInteroNonNegativo(String messaggio) {
-//        return leggiInteroConMinimo(messaggio, 0);
-//    }
-//
-//
-//    public static int leggiInteroConMinimo(String messaggio, int minimo) {
-//        boolean finito = false;
-//        int valoreLetto = 0;
-//        do {
-//            valoreLetto = readNumber(messaggio);
-//            if (valoreLetto >= minimo)
-//                finito = true;
-//            else
-//                System.out.println(ERRORE_MINIMO + minimo);
-//        } while (!finito);
-//
-//        return valoreLetto;
-//    }
-//
-//    public static int leggiIntero(String messaggio, int minimo, int massimo) {
-//        boolean finito = false;
-//        int valoreLetto = 0;
-//        do {
-//            valoreLetto = readNumber(messaggio);
-//            if (valoreLetto >= minimo && valoreLetto <= massimo)
-//                finito = true;
-//            else if (valoreLetto < minimo)
-//                System.out.println(ERRORE_MINIMO + minimo);
-//            else
-//                System.out.println(ERRORE_MASSIMO + massimo);
-//        } while (!finito);
-//
-//        return valoreLetto;
-//    }
-//
-//
-//    public static double leggiDouble(String messaggio) {
-//        boolean finito = false;
-//        double valoreLetto = 0;
-//        do {
-//            System.out.print(messaggio);
-//            try {
-//                valoreLetto = lettore.nextDouble();
-//                finito = true;
-//            } catch (InputMismatchException e) {
-//                System.out.println(ERRORE_FORMATO);
-//                String daButtare = lettore.next();
-//            }
-//        } while (!finito);
-//        return valoreLetto;
-//    }
-//
-//    public static double leggiDoubleConMinimo(String messaggio, double minimo) {
-//        boolean finito = false;
-//        double valoreLetto = 0;
-//        do {
-//            valoreLetto = leggiDouble(messaggio);
-//            if (valoreLetto >= minimo)
-//                finito = true;
-//            else
-//                System.out.println(ERRORE_MINIMO + minimo);
-//        } while (!finito);
-//
-//        return valoreLetto;
-//    }
-//
-//
-//    public static boolean yesOrNo(String messaggio) {
-//        String mioMessaggio = messaggio + "(" + RISPOSTA_SI + "/" + RISPOSTA_NO + ")";
-//        char valoreLetto = leggiUpperChar(mioMessaggio, String.valueOf(RISPOSTA_SI) + String.valueOf(RISPOSTA_NO));
-//
-//        if (valoreLetto == RISPOSTA_SI)
-//            return true;
-//        else
-//            return false;
-//    }
-//
-//}
+package Utility;
+
+import java.util.*;
+
+public class Reader {
+
+    private static Scanner reader = buildScanner();
+
+    private final static String WARNING_THE_INPUT_IS_IN_THE_WRONG_FORMAT = "Warning! The input is in the wrong format";
+    private final static String WARNING_THE_VALUE_MUST_BE_GREATER_OR_EQUAL_TO = "Warning! The value must be greater or equal to ";
+    private final static String WARNING_THE_INPUT_IS_EMPTY = "Warning! The input is empty";
+    private final static String WARNING_THE_VALUE_MUST_BE_LOWER_OR_EQUAL_TO = "Warning! The value must be lower or equal to ";
+    private final static String WARNING_THE_ALLOW_CHARACTERS_ARE = "Warning! The allow characters are: ";
+
+    private final static char YES = 'S';
+    private final static char NO = 'N';
+
+
+    private static Scanner buildScanner() {
+        Scanner create = new Scanner(System.in);
+        //create.useDelimiter(System.getProperty("line.separator"));
+        create.useDelimiter(System.lineSeparator() + "|\n");
+        return create;
+    }
+
+    public static String ReadString(String message) {
+        System.out.print(message);
+        return reader.next();
+    }
+
+    public static String readNotEmpityString(String message) {
+        boolean finished = false;
+        String read = null;
+        do {
+            read = ReadString(message);
+            read = read.trim();
+            if (read.length() > 0)
+                finished = true;
+            else
+                System.out.println(WARNING_THE_INPUT_IS_EMPTY);
+        } while (!finished);
+
+        return read;
+    }
+
+    public static char leggiChar(String message) {
+        boolean finished = false;
+        char valRead = '\0';
+        do {
+            System.out.print(message);
+            String lettura = reader.next();
+            if (lettura.length() > 0) {
+                valRead = lettura.charAt(0);
+                finished = true;
+            } else {
+                System.out.println(WARNING_THE_INPUT_IS_EMPTY);
+            }
+        } while (!finished);
+        return valRead;
+    }
+
+    public static char leggiUpperChar(String message, String allowed) {
+        boolean finished = false;
+        char valRead = '\0';
+        do {
+            valRead = leggiChar(message);
+            valRead = Character.toUpperCase(valRead);
+            if (allowed.indexOf(valRead) != -1)
+                finished = true;
+            else
+                System.out.println(WARNING_THE_ALLOW_CHARACTERS_ARE + allowed);
+        } while (!finished);
+        return valRead;
+    }
+
+
+    public static int readNumber(String message) {
+        boolean finished = false;
+        int valRead = 0;
+        do {
+            System.out.print(message);
+            try {
+                valRead = reader.nextInt();
+                finished = true;
+            } catch (InputMismatchException e) {
+                System.out.println(WARNING_THE_INPUT_IS_IN_THE_WRONG_FORMAT);
+                String trash = reader.next();
+            }
+        } while (!finished);
+        return valRead;
+    }
+
+
+    public static int leggiIntero(String message, int minimo, int massimo) {
+        boolean finito = false;
+        int valoreLetto = 0;
+        do {
+            valoreLetto = readNumber(message);
+            if (valoreLetto >= minimo && valoreLetto <= massimo)
+                finito = true;
+            else if (valoreLetto < minimo)
+                System.out.println(WARNING_THE_VALUE_MUST_BE_GREATER_OR_EQUAL_TO + minimo);
+            else
+                System.out.println(WARNING_THE_VALUE_MUST_BE_LOWER_OR_EQUAL_TO + massimo);
+        } while (!finito);
+
+        return valoreLetto;
+    }
+
+
+    public static double readDouble(String message) {
+        boolean finito = false;
+        double valoreLetto = 0;
+        do {
+            System.out.print(message);
+            try {
+                valoreLetto = reader.nextDouble();
+                finito = true;
+            } catch (InputMismatchException e) {
+                System.out.println(WARNING_THE_INPUT_IS_IN_THE_WRONG_FORMAT);
+                String trash = reader.next();
+            }
+        } while (!finito);
+        return valoreLetto;
+    }
+
+
+    public static boolean yesOrNo(String message) {
+        String myMessage = message + "(" + YES + "/" + NO + ")";
+        char valoreLetto = leggiUpperChar(myMessage, String.valueOf(YES) + String.valueOf(NO));
+
+        if (valoreLetto == YES)
+            return true;
+        else
+            return false;
+    }
+
+}
